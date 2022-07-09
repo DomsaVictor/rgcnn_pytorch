58057521ba31cc3d0cd6a20569c352421bd9bd81
Domșa Victor <domsavictor@gmail.com>
2022-07-09 12:31:58 +0300

    Fixed dataset creation to include categories

--- JurnalCode/DatasetCreation.py
+++ JurnalCode/DatasetCreation.py
@@ -63,18 +63,28 @@ def create_dataset(dataset, transfrom, num_points=2048):
     
 def save_dataset(root, transform, save_path, categories=None, split="train", include_normals=True):
         dataset = ShapeNet(root=root, categories=categories, split=split, include_normals=include_normals, transform=transform)
+        
+        all_categories = sorted(["Airplane", "Bag", "Cap", "Car", "Chair", "Earphone",
+                "Guitar", "Knife", "Lamp", "Laptop", "Motorbike", "Mug",
+                "Pistol", "Rocket", "Skateboard", "Table"])
+        
         if not os.path.isdir(str((save_path/split).resolve())):
             os.makedirs(str((save_path/split)).resolve())
+        if categories is None:
+            categories = range(16)
+        for category in categories:
+            if not os.path.isdir(str((save_path/split/all_categories[category]).resolve())):
+                os.makedirs(str((save_path/split/all_categories[category]).resolve()))
         for i, data in enumerate(dataset):
             name = f"{i}"
             lbl_name = f"{name}.npy"
             pcd_name = f"{name}.pcd"
-            
+            category_name = all_categories[int(data.category)]
             pcd = o3d.geometry.PointCloud(o3d.utility.Vector3dVector(data.pos))
             pcd.normals = o3d.utility.Vector3dVector(data.x)
             
-            o3d.io.write_point_cloud(str((save_path/split/pcd_name).resolve()), pcd)
-            np.save(str((save_path/split/lbl_name).resolve()), data.y)
+            o3d.io.write_point_cloud(str((save_path/split/category_name/pcd_name).resolve()), pcd)
+            np.save(str((save_path/split/category_name/lbl_name).resolve()), data.y)
             
 if __name__ == '__main__':
     # noise_transform([0, 0.01, 0.02, 0.05, 0.1])
@@ -82,9 +92,10 @@ if __name__ == '__main__':
     num_points = 2048
     sigma = 0.05
     transform = Compose([FixedPoints(num_points), utils.GaussianNoiseTransform(0, sigma)])
-    category = "Airplane"
-    # save_dataset(root=imports.dataset_path + "/ShapeNet", transform=transform, 
-    #              save_path=Path(f"{imports.dataset_path}/Journal/ShapeNetCustom/Gaussian_{num_points}_{sigma}/"), categories=category, split="train")
+    category = None
+    save_dataset(root=imports.dataset_path + "/ShapeNet", transform=transform,
+                 save_path=Path(f"{imports.dataset_path}/Journal/ShapeNetCustom/Gaussian_{num_points}_{sigma}/"), 
+                 categories=category, split="train")
         
     dataset = FilteredShapeNet(Path(f"{imports.dataset_path}/Journal/ShapeNetCustom/Gaussian_{num_points}_{sigma}/"), folder="train")
     colors = np.array([[1,0,0], [0,1,0], [0,0,1], [1,0,1]])
